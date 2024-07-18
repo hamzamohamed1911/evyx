9740db91c8f991fa7daf7e9ee49df774ca49462f
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-07-18 13:41:59 +0300

    enahnce

--- app/components/ui/HomeForm.jsx
+++ app/components/ui/HomeForm.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from 'react';
 
 const HomeForm = () => {
@@ -9,52 +9,52 @@ const HomeForm = () => {
     setTimeout(() => {
       setIsSubmitting(false);
     }, 2000); 
-  };
+  };;
 
   return (
-    <div className="md:w-[398px] w-[388px] md:h-[372px] h-auto lg:p-6 md:p-4 p-3 bg-[#2F4E8E] bg-opacity-70 relative rounded-lg shadow-lg text-white">
-      <h1 className="lg:text-3xl text-2xl font-bold lg:my-5 md:my-3 sm:my-2 my-1">تواصل معنا</h1>
+    <div className="w-full md:w-[398px] h-auto lg:p-6 md:p-4 p-3 bg-[#2F4E8E] bg-opacity-70 relative rounded-lg shadow-lg text-white">
+      <h1 className="text-2xl lg:text-3xl font-bold my-1 sm:my-2 md:my-3 lg:my-5">تواصل معنا</h1>
       <form
         name="contact-form"
         method="post"
         action="https://script.google.com/macros/s/AKfycbxV7Mrl_Ou1mSsdOj9mJDIb80y2fXXYJCy_YZStZrppzgUlsGQcK4EO6LsfwfRdLK8OpQ/exec"
         onSubmit={handleSubmit}
       >
-        <div className="lg:mb-5 mb-4 lg:text-lg text-md ">
-          <label htmlFor="name" className="block lg:my-2 my-1">الاسم</label>
+        <div className="mb-4 lg:mb-5 text-md lg:text-lg">
+          <label htmlFor="name" className="block my-1 lg:my-2">الاسم</label>
           <input
             type="text"
             id="name"
             name="name"
-            className="w-full text-black h-[44px] px-3 py-2 mt-1  border rounded-md focus:outline-none focus:ring-2"
+            className="w-full h-[44px] px-3 py-2 mt-1 text-black border rounded-md focus:outline-none focus:ring-2"
             required
             aria-label="الاسم"
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="phone" className="block  lg:my-2 my-1">رقم الهاتف</label>
+          <label htmlFor="phone" className="block my-1 lg:my-2">رقم الهاتف</label>
           <input
             type="text"
             id="phone"
             name="phone"
-            className="w-full text-black h-[44px] px-3 py-2 mt-1  border rounded-md focus:outline-none focus:ring-2"
+            className="w-full h-[44px] px-3 py-2 mt-1 text-black border rounded-md focus:outline-none focus:ring-2"
             required
             aria-label="رقم الهاتف"
           />
         </div>
-        <div className="flex justify-end items-center ">
-        <button
-  type="submit"
-  className="
-  px-6 lg:py-3 py-2 gap-2 flex items-center bg-[#DB965E] text-white font-medium rounded-md hover:bg-[#f2a86c] transition duration-300 lg:text-xl md:text-lg sm:text-md text-sm
-  "
-  disabled={isSubmitting}
->
+        <div className="flex justify-end items-center">
+          <button
+            type="submit"
+            className="
+              flex items-center gap-2 px-4 py-2 lg:px-6 lg:py-3 text-sm sm:text-md md:text-lg lg:text-xl bg-[#DB965E] text-white font-medium rounded-md hover:bg-[#f2a86c] transition duration-300
+            "
+            disabled={isSubmitting}
+          >
             {isSubmitting ? (
-              <svg
-                className="animate-spin h-5 w-5 mr-3 border-t-2 border-white rounded-full inline-block"
-                viewBox="0 0 24 24"
-              ></svg>
+               <svg
+               className="inline-block w-5 h-5 mr-3 animate-spin border-t-2 border-white rounded-full"
+               viewBox="0 0 24 24"
+             ></svg>
             ) : (
               'دعنا نتصل بك'
             )}
@@ -66,4 +66,3 @@ const HomeForm = () => {
 };
 
 export default HomeForm;
-
