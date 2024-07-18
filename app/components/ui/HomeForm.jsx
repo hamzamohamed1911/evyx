f18564822b71f7275d6fa18d569dc449b7a71006
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-07-18 12:39:16 +0300

    enahnce more

--- app/components/ui/HomeForm.jsx
+++ app/components/ui/HomeForm.jsx
@@ -13,7 +13,7 @@ const HomeForm = () => {
 
   return (
     <div className="md:w-[398px] w-[388px] md:h-[360px] h-[340px] p-7 bg-[#2F4E8E] bg-opacity-70 relative rounded-lg shadow-lg text-white">
-      <h1 className="lg:text-2xl text-xl font-bold lg:my-4 md:my-3 sm:my-2 my-2">تواصل معنا</h1>
+      <h1 className="lg:text-2xl text-xl font-bold lg:my-4 md:my-3 sm:my-2 my-1">تواصل معنا</h1>
       <form
         name="contact-form"
         method="post"
