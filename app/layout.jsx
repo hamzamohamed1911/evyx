dcef669ac3f43db01c25a1bb69bbf92edae23100
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-07-18 16:26:07 +0300

    new

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body style={{ fontFamily: 'Almarai, sans-serif' }}>
+      <body className='bg-[#fffdfc]' style={{ fontFamily: 'Almarai, sans-serif' }}>
        
         {children}
       </body>
