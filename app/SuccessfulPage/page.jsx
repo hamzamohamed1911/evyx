ddb1e4373d76b45d80d0a465bda32797fa8d8a83
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-07-21 15:41:55 +0300

    new

--- app/SuccessfulPage/page.jsx
+++ app/SuccessfulPage/page.jsx
@@ -7,15 +7,23 @@ const SuccessPage = () => {
     <section className="min-h-screen flex items-center justify-center ">
       <div className="max-w-4xl mx-auto  text-center">
         <div className="flex justify-center mb-8">
-          <Image src={SuccessfulLogo} width={314.1} height={314.1} alt="Success Logo" />
+          <Image
+            src={SuccessfulLogo}
+            width={314.1}
+            height={314.1}
+            alt="Success Logo"
+          />
         </div>
         <h1 className="text-4xl font-bold mb-6">شكرًا لك</h1>
         <p className="text-xl lg:text-3xl font-bold leading-relaxed lg:leading-normal mb-10">
-          نحن نقدر تواصلك معنا وسنقوم بالرد على استفسارك في أقرب وقت ممكن. إذا كان لديك أي استفسارات أخرى، لا تتردد في التواصل معنا.
+          نحن نقدر تواصلك معنا وسنقوم بالرد على استفسارك في أقرب وقت ممكن. إذا
+          كان لديك أي استفسارات أخرى، لا تتردد في التواصل معنا.
         </p>
-        <Link className="inline-block px-6 py-3 bg-[#DB965E] text-white font-medium text-xl rounded-md hover:bg-[#f2a86c] transition duration-300" href="/">
-         
-            الرجوع الى الرئيسية
+        <Link
+          className="inline-block px-6 py-3 bg-[#DB965E] text-white font-medium text-xl rounded-md hover:bg-[#f2a86c] transition duration-300"
+          href="/"
+        >
+          الرجوع الى الرئيسية
         </Link>
       </div>
     </section>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,15 @@
-
 import "./globals.css";
 
 export const metadata = {
   title: "Evyx",
-  description: " نحن نهدف إلى جعل النجاح في متناول كل شركة، بغض النظر عن حجم أعمالك. نحن هنا لنجعلها لامعة في هذا العالم التنافسي، ببساطة نحن هنا لترجمة عملك إلى النجاح ",
+  description:
+    " نحن نهدف إلى جعل النجاح في متناول كل شركة، بغض النظر عن حجم أعمالك. نحن هنا لنجعلها لامعة في هذا العالم التنافسي، ببساطة نحن هنا لترجمة عملك إلى النجاح ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body style={{ fontFamily: 'Almarai, sans-serif' }}>  
-        {children}
-      </body>
+      <body style={{ fontFamily: "Almarai, sans-serif" }}>{children}</body>
     </html>
   );
 }
